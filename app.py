541223107cc1f6a0455f4cfd811dba555e834ac9
armangarg123 <arman.garg123@gmail.com>
2026-03-09 23:42:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ def render_tab(tab):
 if __name__ == "__main__":
     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=8050)
+    app.run(debug=False, host="0.0.0.0", port=10000)
